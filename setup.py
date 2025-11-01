400e9015b518a653c7b27b0dd06163593622befd
Jacinto <jacintoquimua7@hotmail.com>
2025-11-01 11:37:29 -0600

    update version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="divide21env",
-    version="0.1.2",
+    version="0.1.3",
     author="Jacinto Jeje Matamba Quimua",
-    description="A custom Gymnasium-compatible environment for the Divide21 game invented by Jacinto Jeje Matamba Quimua.",
+    description="A custom Gymnasium-compatible environment for the Divide21",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
