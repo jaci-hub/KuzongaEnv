c5d871ac0d882b1f74d9ef58331943a864f791df
Jacinto <jacintoquimua7@hotmail.com>
2025-11-07 11:35:07 -0700

    update tests

--- test_env_run.py
+++ test_env_run.py
@@ -10,8 +10,18 @@ env = gym.make(
     auto_render=True
 )
 
+def manual_reset_test(state):
+    obs, info = env._manual_reset(obs=state)
+    print(info)
+    # test five examples
+    for i in range(5):
+        action = env.action_space.sample()
+        obs, reward, done, trunc, info = env.step(action)
+        print(info)
+        if done or trunc:
+            break
 
-if __name__ == "__main__":
+def reset_test():
     obs, info = env.reset()
     # test five examples
     for i in range(5):
@@ -20,3 +30,18 @@ if __name__ == "__main__":
         print(info)
         if done or trunc:
             break
+        
+
+if __name__ == "__main__":
+    # given state
+    state = {
+        "static_number": 19,
+        "dynamic_number": 59,
+        "available_digits_per_rindex": {0: [0, 1, 2, 3, 4, 5, 6, 7, 8], 1: [2, 3, 4, 6, 7, 8, 9]},
+        "players": [{'id': 0, 'score': -13, 'is_current_turn': 1}],
+        "player_turn": 0
+    }
+    
+    # manual_reset_test(state)
+    
+    reset_test()
